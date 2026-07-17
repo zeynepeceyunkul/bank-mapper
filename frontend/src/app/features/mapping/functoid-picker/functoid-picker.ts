a74e94fe4420973c10f257921a6a89f7e007ffff
Zeynep Ece Yünkül <eceyunkul44@gmail.com>
2026-07-17 11:36:12 +0300

    Faz 10: Node canvas'i graph modeline gecirildi - palet ten surukle-birak ile functoid ekleme, port-to-port baglanti, parametre paneli, sabit deger node'u

--- frontend/src/app/features/mapping/functoid-picker/functoid-picker.ts
+++ frontend/src/app/features/mapping/functoid-picker/functoid-picker.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FunctoidDefinition } from '../../../core/models/functoid.model';
 
 @Component({
   selector: 'app-functoid-picker',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './functoid-picker.html',
   styleUrl: './functoid-picker.scss',
 })
-export class FunctoidPicker {}
+export class FunctoidPicker {
+  @Input() definitions: FunctoidDefinition[] = [];
+
+  onDragStart(event: DragEvent, code: string): void {
+    event.dataTransfer?.setData('text/functoid-code', code);
+    if (event.dataTransfer) {
+      event.dataTransfer.effectAllowed = 'copy';
+    }
+  }
+}
